64b7436c95bb14d9869642e0d819ada1cbb812e3
Chris Daykin <103705442+MusicMan888@users.noreply.github.com>
2023-11-07 11:51:52 -0500

    Major part of this project is complete
    I completed most of the interface, creation of the grid, just some
    clean up, color and font choices and the footer remain incomplete. I
    spent so much time struggling to make this work I didn't make many
    commits.

--- script.js
+++ script.js
@@ -1,10 +1,130 @@
 const container = document.querySelector('.gridCnt');
+const userGrdSize = document.querySelector('.user-in');
+const submit = document.querySelector('.submit');
+const u_prompt = document.querySelector('.prompt');
+const userCopy = document.querySelector('.user-copy');
+const clearBtn = document.querySelector('.clear-btn');
 
-// let divNmbr = 256;
 
-// for (let i = 0; i < divNmbr; i++) {
-//   const divs = document.createElement('div');
-//   divs.classList.add('items');
-//   // divs.innerHTML = i + 1
-//   container.appendChild(divs);
-// }
+userGrdSize.addEventListener('focus', usrPrompt);
+userGrdSize.addEventListener('keyup', dispGridDm);
+submit.addEventListener('click', createGrid);
+clearBtn.addEventListener('click', clrGrid);
+
+
+
+// ----------------program--------------------
+createGrid();
+drawEnable(); 
+// ---------------functions-------------------
+
+function dispGridDm(){
+  
+  let yDm = userGrdSize.value;
+  userCopy.textContent = "x " + yDm;
+}
+
+function usrPrompt(){
+  u_prompt.textContent = "Enter A Number Between 10 And 99";
+}
+
+function createGrid(){
+console.log("enter createGrid()")
+  let num = userGrdSize.value;
+  console.log(num);
+  // if num < 0 - works
+  if(num < 0 || num > 99 || isNaN(num)){
+    u_prompt.textContent = "Make Sure It's A Number Between 10 And 99";
+  } else {
+    //clear values
+    u_prompt.textContent = "";
+    userCopy.textContent = "";
+    userGrdSize.value = "";
+    container.innerHTML = "";
+
+    //make default 16 x 16 grid 
+    if (num == 0 || num > 99 || num == ""){
+      u_prompt.textContent = "Default Grid Is 16 x 16";
+      for(let a = 0; a < 16; a++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for(let b = 0; b < 16; b++){
+          let column = document.createElement('div');
+          column.classList.add('column');
+          row.appendChild(column);
+        }
+      }
+    } else {
+        if(num < 10 || num > 99 || isNaN(num)){
+          u_prompt.textContent = "Make Sure It's A Number Between 10 And 99";
+          for(let a = 0; a < 16; a++){
+            let row = document.createElement('div');
+            row.classList.add('row');
+            container.appendChild(row);
+            for(let b = 0; b < 16; b++){
+              let column = document.createElement('div');
+              column.classList.add('column')
+              row.appendChild(column);
+            }
+          }
+        } else {
+          u_prompt.textContent = "User Grid Is " + num + " x " + num;
+          for (let a = 0; a < num; a++){
+            let row = document.createElement('div');
+            row.classList.add('row');
+            container.appendChild(row);
+            for(let b = 0; b < num; b++){
+              let column = document.createElement('div');
+              column.classList.add('column')
+              row.appendChild(column);
+            }
+          }
+        } 
+      
+      }
+  }
+  drawEnable();
+} 
+
+function drawEnable(){
+  let columns = document.getElementsByClassName("column");
+  for (let i = 0; i < columns.length; i++){
+    columns[i].addEventListener('mouseover', changeColor);
+  }
+}
+
+function changeColor() {
+  let blkRadio = document.getElementById('black');
+  let redRadio = document.getElementById('red');
+  let bluRadio = document.getElementById('blue');
+  let grnRadio = document.getElementById('green');
+  let rndRadio = document.getElementById('random');
+  let eraRadio = document.getElementById('erase');
+
+  if (blkRadio.checked){
+    this.style.backgroundColor = '#2e2b2b';
+    } 
+    else if(redRadio.checked){
+      this.style.backgroundColor = '#da2d2d';
+    }
+    else if(bluRadio.checked){
+      this.style.backgroundColor = '#2214e2';
+    }
+    else if(grnRadio.checked){
+      this.style.backgroundColor = '#07641c';
+    }
+    else if(eraRadio.checked){
+      this.style.backgroundColor = '';
+    }
+    else if(rndRadio.checked){
+      let randomColor = Math.floor(Math.random()*16777215).toString(16);
+      this.style.backgroundColor = "#" + randomColor;
+    }
+}
+function clrGrid(){
+  let columns = document.getElementsByClassName("column");
+  for (let i = 0; i < columns.length; i++){
+    columns[i].style.backgroundColor= '';
+  }
+}
